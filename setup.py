e79af7b722631fa4a305aad36309ef3d93f2a7a0
Thomas Petig <thomas@petig.eu>
2018-12-15 13:12:39 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="riksbanken_pkg",
+    name="riksbank",
     version="0.0.1",
     author="Thomas Petig",
     author_email="thomas@petig.eu",
     description="Read from riksbankens SOAP API to pandas",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/thpe/riksbanken",
+    url="https://github.com/thpe/riksbank",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
