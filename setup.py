528435599f052b692a852d88c56b8e55dd15cd51
Thomas Petig <thomas@petig.eu>
2018-12-15 13:22:23 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="riksbank",
-    version="0.0.1",
+    version="0.0.2",
     author="Thomas Petig",
     author_email="thomas@petig.eu",
     description="Read from riksbankens SOAP API to pandas",
